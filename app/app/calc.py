ec62abb81d90f13a53eef98c3012f9a54f064d9d
Avinash Katore <2420265@tcs.com>
2024-04-18 15:39:33 +0530

    psycopg2 package and postgres database configure

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def substract(x, y):
+    """Substract x from y and return value"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+    
+""" Sample test """
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module """
+    
+    def test_add_numbers(self):
+        """ Test that two numbers are added together """
+        res = calc.add(5,6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_substract_numbers(self):
+        """ Test that values are substracted and returned """
+        res = calc.substract(15,10)
+        
+        self.assertEqual(res, 5)
